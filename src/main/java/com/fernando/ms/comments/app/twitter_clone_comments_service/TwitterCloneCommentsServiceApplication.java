d9dc9148c7bada69778292ff6e3ed6a0fac99efa
Fernando <asialer05@hotmail.com>
2025-01-14 13:19:42 -0500

    Proyecto base

---
+++ src/main/java/com/fernando/ms/comments/app/twitter_clone_comments_service/TwitterCloneCommentsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.ms.comments.app.twitter_clone_comments_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterCloneCommentsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterCloneCommentsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernando/ms/comments/app/twitter_clone_comments_service/TwitterCloneCommentsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.ms.comments.app.twitter_clone_comments_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCloneCommentsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
