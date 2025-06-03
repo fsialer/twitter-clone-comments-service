57a22e1b6f64c1fb3aa15a536f800a6d1e8d3af7
Fernando <asialer05@hotmail.com>
2025-06-02 22:47:18 -0500

    [bugfix] change typedata string to localdatetime

--- src/main/java/com/fernando/ms/comments/app/infraestructure/adapter/input/rest/models/request/CreateCommentRequest.java
+++ src/main/java/com/fernando/ms/comments/app/infraestructure/adapter/input/rest/models/request/CreateCommentRequest.java
@@ -1,7 +1,10 @@
 package com.fernando.ms.comments.app.infraestructure.adapter.input.rest.models.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -13,7 +16,7 @@ public class CreateCommentRequest {
     private String content;
     @NotBlank(message = "Field postId cannot be null or blank")
     private String postId;
-    @NotBlank(message = "Field dateComment cannot be null")
-    private String dateComment;
+    @NotNull(message = "Field dateComment cannot be null")
+    private LocalDateTime dateComment;
     private String parentComment;
 }
